a50696eedb95831337d96ceca26fcd08104c8d53
h9661 <guscksdn123@gmail.com>
2023-10-30 20:12:45 +0900

    [Add] Posts entity 생성

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,8 +1,22 @@
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'posts' })
 export class Post {
+  @Column({ primary: true, generated: true })
   id: number;
+
+  @Column({ length: 500 })
   author: string;
+
+  @Column('text')
   content: string;
+
+  @Column({ length: 100 })
   title: string;
+
+  @Column({ default: new Date() })
   createdAt: Date;
+
+  @Column({ default: new Date() })
   updatedAt: Date;
 }
