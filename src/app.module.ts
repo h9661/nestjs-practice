9053bc167bfca5030da0cbad9165110556675d95
h9661 <guscksdn123@gmail.com>
2023-10-31 14:15:57 +0900

    [Feat] User entities에 추가 및 User 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './posts/entities/post.entity';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
     PostsModule,
+    UsersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: '127.0.0.1',
@@ -17,7 +20,7 @@ import { Post } from './posts/entities/post.entity';
       username: 'postgres',
       password: 'postgres',
       database: 'postgres',
-      entities: [Post],
+      entities: [Post, User],
       synchronize: true,
     }),
   ],
