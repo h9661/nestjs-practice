37d18a880182eef71a7c0e2314a5432e38af1437
h9661 <guscksdn123@gmail.com>
2023-10-31 17:42:07 +0900

    [Add] auth module 추가

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  signToken(user: Pick<User, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: 'secret',
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
+
+  loginUser(user: Pick<User, 'email' | 'id'>) {
+    const accessToken = this.signToken(user, false);
+    const refreshToken = this.signToken(user, true);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async authenticateWithEmailAndPassword(
+    user: Pick<User, 'email' | 'password'>,
+  ) {
+    const exUser = await this.usersService.getUserByEmail(user.email);
+
+    if (!exUser) {
+      throw new UnauthorizedException('not exist user');
+    }
+
+    const isMatch = await bcrypt.compare(user.password, exUser.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('wrong password');
+    }
+
+    return exUser;
+  }
+
+  async loginWithEmail(user: Pick<User, 'email' | 'password'>) {
+    const exUser = await this.authenticateWithEmailAndPassword(user);
+
+    return this.loginUser(exUser);
+  }
+
+  async registerWithEmail(user: Pick<User, 'email' | 'name' | 'password'>) {
+    const hash = await bcrypt.hash(user.password, 12);
+
+    const newUser = await this.usersService.create({
+      ...user,
+      password: hash,
+    });
+
+    return this.loginUser(newUser);
+  }
+}
