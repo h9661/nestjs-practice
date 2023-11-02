8cac9d613a700f91012c92f5922f5d3f8b9c884d
h9661 <guscksdn123@gmail.com>
2023-11-02 14:40:50 +0900

    [Feat] User entity validator 적용 및 register-user.dto 생성

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { IsEmail, IsString, Length } from 'class-validator';
 import { Base } from 'src/common/entities/base.entity';
 import { Post } from 'src/posts/entities/post.entity';
 import { Column, Entity, OneToMany } from 'typeorm';
@@ -10,12 +11,18 @@ enum Role {
 @Entity({ name: 'users' })
 export class User extends Base {
   @Column({ length: 500, unique: true })
+  @IsString()
+  @Length(1, 20)
   name: string;
 
   @Column({ length: 500 })
+  @IsString()
+  @IsEmail()
   email: string;
 
   @Column({ length: 500 })
+  @IsString()
+  @Length(3, 8)
   password: string;
 
   @Column({ type: 'enum', enum: Role, default: Role.USER })
