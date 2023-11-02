b9ecd03495802e1d9ed0dcf98b6353466bb01ed7
h9661 <guscksdn123@gmail.com>
2023-11-02 14:09:24 +0900

    [Feat] class validator 적용

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString({ message: 'Content must be a string' })
   content: string;
+
+  @IsString({ message: 'Title must be a string' })
   title: string;
 }
