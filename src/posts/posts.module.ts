d56008c9e149257ff7d56f4c6fd8808fa4fb69af
h9661 <guscksdn123@gmail.com>
2023-11-02 14:01:09 +0900

    [Chore] 모듈간 의존관계 설정

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,12 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post]), AuthModule, UsersModule],
 })
 export class PostsModule {}
