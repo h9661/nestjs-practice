4011092cb4e8676669b5a89ebf3f0a56e8b20def
h9661 <guscksdn123@gmail.com>
2023-11-02 14:18:30 +0900

    [Feat] 테이블 author 컬럼 제거

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -5,9 +5,6 @@ import { Column, Entity, ManyToOne } from 'typeorm';
 
 @Entity({ name: 'posts' })
 export class Post extends Base {
-  @Column({ length: 500 })
-  author: string;
-
   @Column('text')
   @IsString({ message: 'Content must be a string' })
   content: string;
