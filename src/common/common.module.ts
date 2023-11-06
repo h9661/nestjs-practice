0d68218688b1cda82658c478b7a6a51ccbbd4f6e
h9661 <guscksdn123@gmail.com>
2023-11-06 19:04:40 +0900

    Add MulterModule and imports for AuthModule and UsersModule to CommonModule.

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import { v4 as uuid } from 'uuid';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [],
   providers: [CommonService],
   exports: [CommonService],
+  imports: [
+    MulterModule.register({
+      limits: {
+        fileSize: 1024 * 1024 * 5, // 5MB
+      },
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+          return cb(new Error('Only image files are allowed!'), false);
+        }
+
+        cb(null, true);
+      },
+      storage: {
+        destination: (req, file, cb) => {
+          cb(null, 'uploads/temp');
+        },
+        filename: (req, file, cb) => {
+          cb(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      },
+    }),
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class CommonModule {}
