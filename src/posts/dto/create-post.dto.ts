ff36f5bc0c545f2e2e0d9c9eb53355ace62387dc
h9661 <guscksdn123@gmail.com>
2023-11-06 19:05:29 +0900

    Add image field to CreatePostDto.

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,9 @@
 import { PickType } from '@nestjs/mapped-types';
 import { Post } from '../entities/post.entity';
+import { IsOptional, IsString } from 'class-validator';
 
-export class CreatePostDto extends PickType(Post, ['title', 'content']) {}
+export class CreatePostDto extends PickType(Post, ['title', 'content']) {
+  @IsString()
+  @IsOptional()
+  image?: string;
+}
