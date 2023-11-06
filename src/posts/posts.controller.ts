14d6778be618f09c0e15ab70adade061d2952a1e
h9661 <guscksdn123@gmail.com>
2023-11-06 21:23:12 +0900

    Add image upload functionality and transaction handling to create post endpoint

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,6 +10,7 @@ import {
   Query,
   UseInterceptors,
   UploadedFile,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -18,19 +19,51 @@ import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 import { User } from 'src/users/decorators/user.decorator';
 import { PaginatePostDto } from './dto/paginate-post.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ImageType } from 'src/common/entities/image.entity';
+import { DataSource } from 'typeorm';
+import { PostImageService } from './image/images.service';
 
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(
+    private readonly postsService: PostsService,
+    private readonly dataSource: DataSource,
+    private readonly postImageService: PostImageService,
+  ) {}
 
   @Post('/create')
   @UseGuards(AccessTokenGuard)
   async create(@User('id') id, @Body() createPostDto: CreatePostDto) {
-    if (createPostDto.image) {
-      await this.postsService.createPostImage(createPostDto);
-    }
+    const qr = this.dataSource.createQueryRunner();
+    await qr.connect();
+    await qr.startTransaction();
 
-    return this.postsService.create(createPostDto, id);
+    try {
+      const post = await this.postsService.create(createPostDto, id, qr);
+
+      for (let i = 0; i < createPostDto.images.length; i++) {
+        await this.postImageService.createPostImage(
+          {
+            post,
+            order: i,
+            path: createPostDto.images[i],
+            type: ImageType.POST_IMAGE,
+          },
+          qr,
+        );
+      }
+
+      await qr.commitTransaction();
+      return this.postsService.findOne(post.id);
+    } catch (e) {
+      await qr.rollbackTransaction();
+
+      throw new InternalServerErrorException(
+        '게시글을 생성하는데 실패했습니다.',
+      );
+    } finally {
+      qr.release();
+    }
   }
 
   @Get()
