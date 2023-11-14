2909e6f21373162154c7a2a5c2288ecacbe1f00c
h9661 <guscksdn123@gmail.com>
2023-11-14 23:07:58 +0900

    Add createChat method to ChatsService

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Chat } from './entity/chats.entity';
+import { Repository } from 'typeorm';
+import { CreateChatDto } from './dto/create-chat.dto';
 
 @Injectable()
-export class ChatsService {}
+export class ChatsService {
+  constructor(
+    @InjectRepository(Chat) private readonly chatsRepository: Repository<Chat>,
+  ) {}
+
+  async createChat(dto: CreateChatDto) {
+    const chat = await this.chatsRepository.save({
+      users: dto.userIds.map((id) => ({ id })),
+    });
+
+    return this.chatsRepository.findOne({
+      where: { id: chat.id },
+    });
+  }
+}
