b86a48efbd10f9fc03615cc8e4954bd9e435031b
h9661 <guscksdn123@gmail.com>
2023-11-14 23:11:42 +0900

    Add CommonService to imports in ChatsModule

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,10 +4,11 @@ import { ChatsController } from './chats.controller';
 import { ChatsGateway } from './chats.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from './entity/chats.entity';
+import { CommonService } from 'src/common/common.service';
 
 @Module({
   controllers: [ChatsController],
   providers: [ChatsService, ChatsGateway],
-  imports: [TypeOrmModule.forFeature([Chat])],
+  imports: [TypeOrmModule.forFeature([Chat]), CommonService],
 })
 export class ChatsModule {}
