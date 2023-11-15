59d62e6af5f8685eff995f0137d416f99e3e92fe
h9661 <guscksdn123@gmail.com>
2023-11-15 11:50:36 +0900

    [Feat] Add pagination to chats endpoint

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -3,12 +3,26 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Chat } from './entity/chats.entity';
 import { Repository } from 'typeorm';
 import { CreateChatDto } from './dto/create-chat.dto';
+import { CommonService } from 'src/common/common.service';
+import { PaginateChatDto } from './dto/paginate-chat.dto';
 
 @Injectable()
 export class ChatsService {
   constructor(
     @InjectRepository(Chat) private readonly chatsRepository: Repository<Chat>,
+    private readonly commonService: CommonService,
   ) {}
+
+  paginateChats(dto: PaginateChatDto) {
+    return this.commonService.paginate<Chat>(
+      dto,
+      this.chatsRepository,
+      {
+        relations: ['users'],
+      },
+      'chats',
+    );
+  }
 
   async createChat(dto: CreateChatDto) {
     const chat = await this.chatsRepository.save({
